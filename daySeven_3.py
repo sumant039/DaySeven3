8361ddb397a9da9b82b54f32cf44efc1ea91a7ef
sumant singh <48150206+sumant039@users.noreply.github.com>
2019-07-03 13:47:54 +0530

    DS Code For Practice

---
+++ daySeven_3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+#Public attr
+
+class employee:
+    def __init__(self, name, sal):
+        self.name=name
+        self.salary=sal
+
+
+
+e1=Employee("Kiran",10000)
+print e1.salary
+
+#protected
+#Derived class can access
+#_name
+
+#Private
+#__name
+
+
+
+
+
